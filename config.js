d209276cbba344395618cb427bc048cfe2353a07
RanggaKelaz <127925789+RanggaKelaz@users.noreply.github.com>
2025-07-13 05:53:33 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  BOT_TOKEN: 'YOUR_TELEGRAM_BOT_TOKEN',
+  BOT_TOKEN: '7820392212:AAHOdgW2PWq7el8EbI623odGW7Hgcihl2g8',
   OPENAI_API_KEY: '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',
-  OWNER_IDS: ['YOUR_TELEGRAM_USER_ID'], // Array of Telegram user IDs who are owners
+  OWNER_IDS: ['8067565681'], // Array of Telegram user IDs who are owners
   PREMIUM_USERS: [], // Array of Telegram user IDs who are premium users
 };
 
